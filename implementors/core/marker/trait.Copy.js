b6723f7698d09b7bb792701d1a5cf101ae113fb4
Travis CI User <travis@example.org>
2020-07-31 20:05:50 +0000

    Documentation for Kimundi/rustc-version-rs@1d692a9

---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["rustc_version"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"rustc_version/enum.Channel.html\" title=\"enum rustc_version::Channel\">Channel</a>","synthetic":false,"types":["rustc_version::Channel"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ source-files.js
@@ -0,0 +1,5 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["rustc_version"] = {"name":"","files":["errors.rs","lib.rs"]};
+sourcesIndex["semver"] = {"name":"","files":["lib.rs","version.rs","version_req.rs"]};
+sourcesIndex["semver_parser"] = {"name":"","files":["common.rs","lib.rs","range.rs","recognize.rs","version.rs"]};
+createSourceSidebar();
